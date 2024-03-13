546b92972c01a6bcf769848ba5bf5d70aacba334
ednaleiden <ednadupont@hotmail.com>
2024-03-13 12:51:35 -0500

    finish project of crud in thymleaf

--- src/main/java/com/projectSpring/controller/CursoController.java
+++ src/main/java/com/projectSpring/controller/CursoController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -54,6 +55,32 @@ public class CursoController {
         }catch (Exception e) {
             redirectAttributes.addAttribute("message",e.getMessage());
 
+        }
+        return "redirect:/cursos";
+    }
+
+    @GetMapping("/cursos/editar/{id}")
+    public String editarCurso(@PathVariable Integer id, Model model , RedirectAttributes redirectAttributes){
+        try {
+           Curso curso = cursoRepository.findById(id).get();
+           model.addAttribute("curso", curso);
+           model.addAttribute("pageTitle","Editar curso : "+ id);
+          return "curso_form";
+
+        }catch (Exception e) {
+            redirectAttributes.addFlashAttribute("message",e.getMessage());
+
+        }
+        return "redirect:/cursos";
+    }
+
+    @GetMapping("/cursos/eliminar/{id}")
+    public String eliminarCursos(@PathVariable Integer id, Model model , RedirectAttributes redirectAttributes){
+        try {
+            cursoRepository.deleteById(id);
+            redirectAttributes.addAttribute("message","curso eliminado con exito");
+        } catch (Exception e) {
+            redirectAttributes.addFlashAttribute("message",e.getMessage());
         }
         return "redirect:/cursos";
     }
